4d955ced18bdba9a3379a5a3e061313f7acb25ad
GregorioCorrea <110120893+GregorioCorrea@users.noreply.github.com>
2022-11-08 22:06:02 -0300

    Cambios en routes/web.php: De acuerdo con la quinta lección de Platzi, cambió la forma en la que se declaran las rutas.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'UserController@index');
-Route::post('users', 'UserController@store')->name('users.store');
-Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+Route::get('/', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
